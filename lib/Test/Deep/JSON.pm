84fb1dbe0a7cc96e58ed56a3cbf840ffb09cb950
motemen <motemen@gmail.com>
2012-10-02 19:56:44 +0900

    doc

--- lib/Test/Deep/JSON.pm
+++ lib/Test/Deep/JSON.pm
@@ -42,19 +42,49 @@ __END__
 
 =head1 NAME
 
-Test::Deep::JSON - 
+Test::Deep::JSON - Compare JSON with Test::Deep
 
 =head1 SYNOPSIS
 
+  use Test::Deep;
   use Test::Deep::JSON;
 
+  cmp_deeply {
+      payload => '{"a":1}',
+  }, {
+      payload => json({ a => 1 }),
+  };
+
 =head1 DESCRIPTION
+
+Test::Deep::JSON provides C<json($expected)> function to expect that
+target can be parsed as a JSON string and matches (with C<cmp_deeply>) with
+I<$expected>.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item json($expected)
+
+Exported by default.
+
+$expected can be anything that C<Test::Deep> recognizes.
+
+This parses the data as a JSON string, and compares the parsed object
+and I<$expected> by C<Test::Deep> functionality.
+
+Fails if JSON parsing is failed.
+
+=back
 
 =head1 AUTHOR
 
 motemen E<lt>motemen@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<Test::Deep>
 
 =head1 LICENSE
 
